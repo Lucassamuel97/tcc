9ec7fb380d576e3172725ca0e921a81236fa342f
Lucas Samuel <lukassamuka88@gmail.com>
2021-12-06 14:21:33 -0300

    add page error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Not Found</title>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 100;
+                margin: 0;
+            }
+            img{
+                width: 60vh;
+                margin-top: 10vh;
+            }
+            .image{
+                width: 100%;
+            }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .code {
+                border-right: 2px solid;
+                font-size: 26px;
+                padding: 0 15px 0 15px;
+                text-align: center;
+            }
+            .message {
+                font-size: 18px;
+                text-align: center;
+                padding: 10px;
+            }
+            .link{
+                margin-
+            }
+        </style>
+    </head>
+    <body>
+        <div class="position-ref full-height">
+            
+            <div class="flex-center image">
+                <img src="{{url('assets/img/tractor404.jpg')}}" alt="trator quebrado">
+            </div>
+            <div class="code">
+                404            
+            </div>
+            <div class="message">
+                Pagina não encontrada            
+            </div>
+            <div class="flex-center">
+                <a href='{{url("/")}}'> Ir para página inicial</a>
+            </div>
+        </div>
+    </body>
+</html>
