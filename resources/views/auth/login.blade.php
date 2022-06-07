b782f3f8ade9c5a4dd0e6f966eac9d461f00f0c9
Lucas Samuel <lukassamuka88@gmail.com>
2022-06-07 14:04:25 -0300

    Arrumado bugs layout e favicon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="shortcut icon" href="{{url('assets/img/favicon.png')}}">
+    
     <title>Manutenções Preventivas</title>
 
     <!-- Fonts -->
